6dbda412034adb407c2933618f3302c1c1458cec
Antonio <amcbd89@gmail.com>
2019-09-06 12:13:06 +0100

    Initial commit

---
+++ app/javascript/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+function HelloWorld(){
+
+  return (
+    <React.Fragment>
+      <div>
+        Hi, This is a React component
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default HelloWorld
