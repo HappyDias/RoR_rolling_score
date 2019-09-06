70036c18218e55cbfcb08c3629eeb190f80ac3d7
Antonio <amcbd89@gmail.com>
2019-09-06 15:58:30 +0100

    Added checks for parameters sent and instantiates TimeSeries

--- app/controllers/api/rollscore_controller.rb
+++ app/controllers/api/rollscore_controller.rb
@@ -1,3 +1,5 @@
+require "TimeSeries.rb"
+
 class Api::RollscoreController < ApplicationController
 	skip_before_action :verify_authenticity_token
 	def index
@@ -10,7 +12,14 @@ class Api::RollscoreController < ApplicationController
 		] }.to_json
 	end
 	def post
-
-		render json: params.to_json
+		if params.has_key?(:data) and params.has_key?(:threshold) and params.has_key?(:window)
+			if params[:data].kind_of?(Array) and params[:threshold].kind_of?(Integer) and params[:window].kind_of?(Integer)
+				series = TimeSeries.new(params[:data])
+				series.roll_z_score(5)
+				render json: params.to_json
+			#Here a custom type error should be raised
+			end
+		#Here custom argument missing error should be raised
+		end
 	end
 end
